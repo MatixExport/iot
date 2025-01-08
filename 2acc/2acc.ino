e07de5e2e91d5a27b3f924e839dcf7ed2b86423e
247662 Gieczyski Mateusz <mateusz.gielczynski@outlook.com>
2025-01-08 14:32:20 +0100

    add code comments

--- 2acc/2acc.ino
+++ 2acc/2acc.ino
@@ -65,74 +65,74 @@ void loop()
 
   switch (currentState) {
     case CLOSED:                          
-      if(sum1 < -10){ //starts oppening
+      if(sum1 < -10){                         //starts oppening
           currentState = OPENING1;
           timerRestart(Timer0_Cfg);
       }
       break;
     case OPENING1:
-      openness = -(sum1+10)/21;
-      if(sum1 < -600){            //finnished opening first gate module
+      openness = -(sum1+10)/21;               //calculate openness based on values from acc1
+      if(sum1 < -600){                        //finnished opening first gate module
           currentState = OPENING2;
           step1Time = timerRead(Timer0_Cfg);
-          change = (float)30 / step1Time;     //calibrate change
+          change = (float)30 / step1Time;     //calibrate change besed on speed of opening first gate module
       }
       break;
     case OPENING2:
       openness = timerRead(Timer0_Cfg) * change;
-      if(sum2 < 0 && sum2 > -10){
+      if(sum2 < 0 && sum2 > -10){             //started opening fourth gate module
         currentState = OPENING3;
       }
-      else if(openness >= 80){
+      else if(openness >= 80){                //expected to start opening fourth gate module, but values from acc indicates it is not open
         currentState = ERROR;
       }
       break;
     case OPENING3:
-      openness = -(sum2+10)/21 + 70;
-      if(openness > 100){
+      openness = -(sum2+10)/21 + 70;          //calculate openness based on values from acc2
+      if(openness > 95){                      // fully open gate
         currentState = OPEN;
       }
       break;
     case ERROR:
       Serial.println("error");
       if(sum1 < 150 && sum1 > 50){
-        if(sum2 < 150 && sum2 > 50){    // fully colsed
+        if(sum2 < 150 && sum2 > 50){          // fully colsed
           currentState = CLOSED;
           openness = 0;
         }
       }
-      if(sum1 < -600 && sum2 < -600){ // fully open
+      if(sum1 < -600 && sum2 < -600){         // fully open
         currentState = OPEN;
         openness = 100;
       }
       break;
     case OPEN:
-      if(sum2 > -600){ //starts closing
+      if(sum2 > -550){                        //starts closing
           currentState = CLOSING1;
           timerRestart(Timer0_Cfg);
         }
       break;
 
     case CLOSING1:
-      openness = -(sum2+10)/21 + 70;
-      if(openness < 70){
+      openness = -(sum2+10)/21 + 70;          //calculate openness based on values from acc2
+      if(openness < 70){                      //finnished closing first (from top) gate module
         currentState = CLOSING2;
         step1Time = timerRead(Timer0_Cfg);
-        change = (float)30 / step1Time;
+        change = (float)30 / step1Time;       //calibrate change besed on speed of closing first gate module
       }
       break;
         
     case CLOSING2:
       openness = 100 - timerRead(Timer0_Cfg) * change;
-      if(sum1 > -600){
+      if(sum1 > -550){                        //started closing fourth gate module       
         currentState = CLOSING3;
       }
-      else if(openness <= 30){
+      else if(openness <= 30){                //expected to started closing fourth gate module, but values from acc indicates it is not open
         currentState = ERROR;
       }
       break;
     case CLOSING3:
-      openness = - (sum1+10)/21;
+      openness = - (sum1+10)/21;              //calculate openness based on values from acc2
       if(openness <= 0){
         currentState = CLOSED;
       }
